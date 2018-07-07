230fecfc9b4dccf6126055e82d6bcbd15dbad196
Albert Sanzari <as2222@njit.edu>
2018-07-07 15:44:23 -0400

    Created contact page with thank you reply

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to the Homepage
                 </div>
 
                 <div class="links">
