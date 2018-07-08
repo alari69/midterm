1684847348f408f453ae38945244f1a60a2ab33b
Albert Sanzari <as2222@njit.edu>
2018-07-08 00:45:20 -0400

    About Page test passed.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,7 @@
     <h3>This is my about page for the Mid Term. <br>In near future actions, this page may be filled with more info.<br>At the moment...this is it! Enjoy!</p></h3>
 
 
-
+@endsection
 
 
 
