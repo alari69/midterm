3c8d219aa83e8892bedd52a217e96bbfb8179ca8
Albert Sanzari <as2222@njit.edu>
2018-07-09 12:39:03 -0400

    Created test for yer is n integer. It failed because I used Date. It's accurate. Date is not an Integer

---
+++ tests/Unit/YearIsIntTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class YearIsIntTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testYearIsInt()
+    {
+        $car = Car::find(10);
+
+        $this->assertInternalType('int',$car->year);
+    }
+}
